9b6012b4323a49be49891e783ce1934f05da6a0d
dat-hoang <dathoang@DatHoang.local>
2025-01-10 22:15:03 +0700

    feat: init source

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
